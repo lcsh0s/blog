ebc22eabe406b8dce3a83be06c89e9ad82f06bec
lcsh0s <lucasdc@amo.co>
2025-11-26 17:40:19 +0100

    fix for new domain

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
 	// Absolute URL to the root of your published site, used for generating links and sitemaps.
-	site: 'https://decastro.dev',
+	site: 'https://ae2.rs',
 	// The name of your site, used in the title and for SEO.
 	title: 'Lucas\' Blog',
 	// The description of your site, used for SEO and RSS feed.
